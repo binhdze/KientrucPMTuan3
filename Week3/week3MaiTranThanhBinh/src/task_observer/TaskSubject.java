5507c2f4af51cc91f100e571f960e4105c3930b2
binhdze <binhdzne@gmail.com>
2025-04-08 14:46:39 +0700

    update
    up

---
+++ Week3/week3MaiTranThanhBinh/src/task_observer/TaskSubject.java
@@ -0,0 +1,8 @@
+package task_observer;
+
+public interface TaskSubject {
+    void registerObserver(TaskObserver observer);
+    void removeObserver(TaskObserver observer);
+    void notifyObservers();
+    String getUpdate(TaskObserver observer);
+}
